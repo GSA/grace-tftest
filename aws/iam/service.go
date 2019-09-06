21770fa59c1bd704c3b5bcee1c184ad45683ce5f
Bryan Alexander <bryan.alexander@gsa.gov>
2019-09-06 13:19:41 -0500

    fixes iam/service.go to include all types, adds Policy() method to Alias, adds Attached() method to Role

--- aws/iam/service.go
+++ aws/iam/service.go
@@ -2,17 +2,20 @@ package iam
 
 import (
 	"github.com/GSA/grace-tftest/aws/iam/policy"
+	"github.com/GSA/grace-tftest/aws/iam/role"
 	"github.com/aws/aws-sdk-go/aws/client"
 )
 
 // Service contains all the supported types for IAM
 type Service struct {
 	Policy *policy.Policy
+	Role   *role.Role
 }
 
 // New returns a new *Service
 func New(client client.ConfigProvider) *Service {
 	return &Service{
 		Policy: policy.New(client),
+		Role:   role.New(client),
 	}
 }
