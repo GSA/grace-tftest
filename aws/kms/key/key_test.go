d8b9e5aa0a973550f585496fdd6c5dc5fdf90813
Brian Fitzwater - IDI-C <brian.fitzwater@gsa.gov>
2020-03-17 10:21:36 -0400

    Add kms key by Description

--- aws/kms/key/key_test.go
+++ aws/kms/key/key_test.go
@@ -1,4 +1,4 @@
-package description
+package key
 
 import (
 	"testing"
@@ -7,17 +7,17 @@ import (
 	"github.com/aws/aws-sdk-go/service/kms"
 )
 
-func TestDescription(t *testing.T) {
-	descriptions := []*kms.KeyMetadata{
+func TestKey(t *testing.T) {
+	keys := []*kms.KeyMetadata{
 		{Arn: aws.String("a"), Description: aws.String("b"), KeyId: aws.String("c")},
 		{Arn: aws.String("d"), Description: aws.String("e"), KeyId: aws.String("f")},
 		{Arn: aws.String("g"), Description: aws.String("h"), KeyId: aws.String("c")},
 	}
 
-	New(nil).Arn("a").Name("b").ID("c").Assert(t, descriptions...)
-	New(nil).ID("f").Assert(t, descriptions...)
-	description := New(nil).ID("c").First(t, descriptions...).Selected()
-	if description == nil {
-		t.Errorf("description was nil")
+	New(nil).Arn("a").Description("b").ID("c").Assert(t, keys...)
+	New(nil).ID("f").Assert(t, keys...)
+	key := New(nil).ID("c").First(t, keys...).Selected()
+	if key == nil {
+		t.Errorf("key was nil")
 	}
 }
