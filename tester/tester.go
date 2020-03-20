bc74666ba1697ff5549f4a1023c9e759d736124a
Bryan Alexander <bryan.alexander@gsa.gov>
2020-03-20 08:08:27 -0500

    updates tester to reduce cyclomatic complexity and add some comments

--- tester/tester.go
+++ tester/tester.go
@@ -21,12 +21,28 @@ import (
 	"time"
 )
 
+// Config is used to configure how Tester is executed
 type Config struct {
-	Dir        string
-	Env        map[string]string
-	vars       []string
-	Services   []string
+	// Dir is the directory that houses all of the test directories
+	Dir string
+
+	// Env is a map of environment variables that need to be provided
+	// to each executable by default AWS_REGION, AWS_ACCESS_KEY_ID, and
+	// AWS_SECRET_ACCESS_KEY are provided with dummy values (and us-east-1)
+	Env map[string]string
+
+	// Services is a list of Terraform AWS Provider custom endpoints
+	// https://www.terraform.io/docs/providers/aws/guides/custom-service-endpoints.html
+	// By default all custom endpoints are statically provided but this
+	// provides an option for explicitly listing them
+	Services []string
+
+	// JobsPerCPU is the number of jobs that will be executed in parallel
+	// per CPU. If it is not set, it will default to 1
 	JobsPerCPU int
+
+	// interally used to store parsed ENV variables
+	vars []string
 }
 
 // Run enumerates over each subfolder in the provided directory
@@ -35,11 +51,13 @@ type Config struct {
 // executes the first file ending in _test.go against moto_server on the
 // first available port
 func Run(cfg *Config) error {
+	// validate and update configuration
 	err := prepareConfig(cfg)
 	if err != nil {
 		return err
 	}
 
+	// create job objects from sub-directories
 	jobs, err := buildJobs(cfg.Dir, cfg.vars)
 	if err != nil {
 		return err
@@ -63,14 +81,19 @@ func Run(cfg *Config) error {
 
 	<-done // wait for all jobs to complete
 
+	// enumerate for cleanup separately so any lingering printing
+	// caused by the interrupt or killing the processes is printed
+	// prior to the job report
+	for _, j := range jobs {
+		j.cleanup()
+	}
+
 	// We have either completed all jobs or
 	// an interrupt signal has been received
-	// cleanup all jobs and echo their final
-	// status output to the screen
+	// print their final status output
 	fmt.Printf("\n\n\n\n===== Job Results =====\n")
 	var failed bool
 	for _, j := range jobs {
-		j.cleanup()
 		if j.Err != nil {
 			failed = true
 			fmt.Printf("%-20s%-15s%v\n", j.Name, "FAILED", j.Err)
@@ -79,6 +102,7 @@ func Run(cfg *Config) error {
 		fmt.Printf("%-20s%-15s\n", j.Name, "SUCCESS")
 	}
 
+	// we need to exit non-zero if any job failed
 	if failed {
 		os.Exit(1)
 	}
@@ -92,17 +116,35 @@ func runJobs(done chan struct{}, cfg *Config, jobs []*job) {
 
 	for _, j := range jobs {
 		j := j
+		// store an empty struct (zero memory alloc)
+		// into the free capacity for throttle for each
+		// job that we execute, this will block when we
+		// reach capacity until a job is completed
 		throttle <- struct{}{}
+
+		// waitgroup is needed to prevent the last X
+		// jobs from being prematurely killed where X
+		// is the capacity of the throttle channel
 		wg.Add(1)
+
 		go func() {
+			// run the 'j' job and store the error result
 			j.Err = j.run(cfg.Services)
+			// free one element in the channel
 			<-throttle
+			// decrement waitgroup by one
 			wg.Done()
 		}()
 	}
 
+	// block until all job go routines have completed
+	// this is only hit when we have more throttle capacity
+	// than we do jobs remaining
 	wg.Wait()
-	done <- struct{}{} // all jobs have completed
+
+	// all jobs have completed unblock the done channel so
+	// we can cleanup, report, and exit
+	done <- struct{}{}
 }
 
 func prepareConfig(cfg *Config) error {
@@ -143,35 +185,42 @@ type job struct {
 	Processes    []*exec.Cmd
 }
 
-const urlFmt = "http://localhost:%d"
-
 func (j *job) run(services []string) error {
 	port, err := getPort()
 	if err != nil {
 		return err
 	}
-	j.Env = append(j.Env, "MOTO_PORT="+strconv.Itoa(port))
 
-	moto, err := j.startProcess("moto_server", "-p", strconv.Itoa(port))
+	cleanup, err := j.startMoto(port)
 	if err != nil {
 		return err
 	}
-	defer func() {
-		if moto.ProcessState != nil &&
-			moto.ProcessState.Exited() {
-			return
-		}
-		err = kill(moto)
-		if err != nil {
-			fmt.Printf("[%s]: failed to terminate process: %v", j.Name, err)
-		}
-	}()
+	defer cleanup()
 
 	err = writeProvider(j.ProviderFile, services, port)
 	if err != nil {
 		return err
 	}
 
+	err = j.runTerraform()
+	if err != nil {
+		return err
+	}
+
+	return j.runTest()
+}
+
+const urlFmt = "http://localhost:%d"
+
+func (j *job) startMoto(port int) (func(), error) {
+	// MOTO_PORT should be available as an ENV var to any
+	// process started for this job
+	j.Env = append(j.Env, "MOTO_PORT="+strconv.Itoa(port))
+
+	moto, err := j.startProcess("moto_server", "-p", strconv.Itoa(port))
+	if err != nil {
+		return nil, err
+	}
 	fmt.Printf("[%s]: waiting for moto to start...\n", j.Name)
 	maxRetries := 20
 	for i := 0; i < maxRetries; i++ {
@@ -181,7 +230,7 @@ func (j *job) run(services []string) error {
 
 		var resp *http.Response
 		//nolint: gosec
-		resp, err = http.Get(url)
+		resp, err := http.Get(url)
 		if err == nil {
 			err = resp.Body.Close()
 			if err != nil {
@@ -192,12 +241,18 @@ func (j *job) run(services []string) error {
 		fmt.Printf("[%s]: waiting for moto to start (attempt %d/%d)\n", j.Name, i+1, maxRetries)
 	}
 
-	err = j.runTerraform()
-	if err != nil {
-		return err
-	}
-
-	return j.runTest()
+	return func() {
+		// if the process has already exited there is no point in
+		// continuing, so return to the caller
+		if moto.ProcessState != nil &&
+			moto.ProcessState.Exited() {
+			return
+		}
+		err = kill(moto)
+		if err != nil {
+			fmt.Printf("[%s]: failed to terminate process: %v", j.Name, err)
+		}
+	}, nil
 }
 
 func (j *job) runTerraform() error {
@@ -266,13 +321,6 @@ func (j *job) cleanup() {
 	}
 }
 
-func ignoreNotExistsErr(err error) error {
-	if os.IsNotExist(err) {
-		return nil
-	}
-	return err
-}
-
 func (j *job) cleanupProcesses() {
 	for _, p := range j.Processes {
 		if p.ProcessState != nil &&
@@ -293,6 +341,9 @@ func (j *job) startProcess(path string, args ...string) (*exec.Cmd, error) {
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, j.Env...)
 
+	// grab the output pipes so we can prepend the job
+	// name before each line so it is easier to discern
+	// which message belongs to which job
 	stdoutP, err := cmd.StdoutPipe()
 	if err != nil {
 		return nil, fmt.Errorf("failed to open stdout pipe: %v", err)
@@ -307,6 +358,9 @@ func (j *job) startProcess(path string, args ...string) (*exec.Cmd, error) {
 		return nil, fmt.Errorf("failed to start process: %s -> %v", path, err)
 	}
 
+	// keep up with what processes we have started
+	// so we can clean them up if we get an interrupt
+	// or if something goes badly
 	j.Processes = append(j.Processes, cmd)
 
 	go func() {
@@ -325,12 +379,18 @@ func (j *job) readOutput(stdout, stderr io.Reader) error {
 	stdoutScanner := bufio.NewScanner(stdout)
 	stderrScanner := bufio.NewScanner(stderr)
 
+	// In order to read the output pipes we use a bufio.Scanner
+	// which by default reads a line on each pass so we start
+	// our wrapping func in a go routine for each pipe
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
 	go j.wrapOutput(j.Stdout, stdoutScanner, wg)
 	go j.wrapOutput(j.Stderr, stderrScanner, wg)
+
+	// block until the process has stopped writing to the pipe
 	wg.Wait()
 
+	// if something goes wrong return the error to the caller
 	if err := stdoutScanner.Err(); err != nil {
 		return fmt.Errorf("stdoutScanner failed: %v", err)
 	}
@@ -342,13 +402,24 @@ func (j *job) readOutput(stdout, stderr io.Reader) error {
 }
 
 func (j *job) wrapOutput(out io.Writer, s *bufio.Scanner, wg *sync.WaitGroup) {
+	// Scan pulls one line from the pipe
+	// so we can wrap it with the job name
 	for s.Scan() {
 		_, err := fmt.Fprintf(out, "[%s]: %s\n", j.Name, s.Text())
 		if err != nil {
 			fmt.Printf("failed to Fprintf: %v\n", err)
 		}
 	}
+
+	// decrement the waitgroup
 	wg.Done()
+}
+
+func ignoreNotExistsErr(err error) error {
+	if os.IsNotExist(err) {
+		return nil
+	}
+	return err
 }
 
 //nolint:unparam
@@ -375,6 +446,8 @@ func mapToKeyValueSlice(m map[string]string) []string {
 }
 
 func mapMerge(m1, m2 map[string]string) map[string]string {
+	// make a third map and join the two maps
+	// allowing m2 to overwrite anything in m1
 	m3 := make(map[string]string, len(m1)+len(m2))
 	for k := range m1 {
 		m3[k] = m1[k]
@@ -386,35 +459,47 @@ func mapMerge(m1, m2 map[string]string) map[string]string {
 }
 
 func buildJobs(dir string, env []string) ([]*job, error) {
+	// resolve the absolute path for the
+	// user provided directory
 	base, err := filepath.Abs(dir)
 	if err != nil {
 		return nil, fmt.Errorf("failed to resolve absolute path to %s -> %v", dir, err)
 	}
 
 	var jobs []*job
+
+	// walk the directory tree for the absolute path
 	err = filepath.Walk(base, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return fmt.Errorf("failed to access path: %q -> %v", path, err)
 		}
+		// if it is not a directory or it is the current
+		// directory, then skip it
 		if !info.IsDir() || base == path {
 			return nil
 		}
 
+		// grab all files inside the directory
+		// and return only files ending in _test.go
 		pattern := filepath.Join(path, "*_test.go")
 		matches, err := filepath.Glob(pattern)
 		if err != nil {
 			return fmt.Errorf("failed to list files in: %q -> %v", path, err)
 		}
 
+		// if no _test.go files were found then
+		// this folder does not qualify as a job
 		if len(matches) == 0 {
 			return nil
 		}
 
 		j := &job{
+			// use the last element of the path
+			// as the job name
 			Name:         filepath.Base(path),
 			RootPath:     base,
 			Path:         path,
-			TestFile:     matches[0],
+			TestFile:     matches[0], // use the first _test.go file
 			ProviderFile: filepath.Join(path, "provider.tf"),
 			Env:          env,
 			Err:          errors.New("job not executed"),
@@ -434,6 +519,7 @@ func buildJobs(dir string, env []string) ([]*job, error) {
 }
 
 func getPort() (int, error) {
+	// open a connection on any free ephemeral port
 	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
 	if err != nil {
 		return 0, err
@@ -445,6 +531,8 @@ func getPort() (int, error) {
 	}
 	defer l.Close()
 
+	// grab the port used to open that connection
+	// and return it to the caller
 	if addr, ok := l.Addr().(*net.TCPAddr); ok {
 		return addr.Port, nil
 	}
@@ -452,6 +540,7 @@ func getPort() (int, error) {
 	return 0, fmt.Errorf("failed to get an available port")
 }
 
+// kill is used to terminate a process that has sub-processes
 func kill(cmd *exec.Cmd) error {
 	pid := strconv.Itoa(cmd.Process.Pid)
 	killers := map[string]*exec.Cmd{
@@ -465,6 +554,7 @@ func kill(cmd *exec.Cmd) error {
 	return fmt.Errorf("runtime %s is not supported yet", runtime.GOOS)
 }
 
+// writeProvider writes the provider.tf file for each job
 func writeProvider(path string, services []string, port int) error {
 	data, err := getProvider(services, port)
 	if err != nil {
@@ -477,6 +567,8 @@ func writeProvider(path string, services []string, port int) error {
 	return nil
 }
 
+// defaultServices contains the full list of working services from
+// https://www.terraform.io/docs/providers/aws/guides/custom-service-endpoints.html
 var defaultServices = []string{
 	"accessanalyzer",
 	"acm",
@@ -604,6 +696,7 @@ var defaultServices = []string{
 	"sts",
 }
 
+// getProvider returns the template formatted contents for the provider.tf
 func getProvider(services []string, port int) ([]byte, error) {
 	serviceList := defaultServices
 	if len(services) > 0 {
@@ -628,12 +721,17 @@ provider "aws" {
 	}
 }
 `
+	// add the port to the template data
 	tmpl = fmt.Sprintf(tmpl, port)
+
+	// create a new template and parse the data
 	t, err := template.New("provider").Parse(tmpl)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse template string: %v", err)
 	}
 
+	// execute the template against a buffer so
+	// we can catch any errors
 	var byt bytes.Buffer
 	err = t.Execute(&byt, serviceList)
 	if err != nil {
