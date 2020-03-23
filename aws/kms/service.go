b5d2ce5da17912f1bf91455d59a11d27c4421314
Brian Fitzwater - IDI-C <brian.fitzwater@gsa.gov>
2020-03-23 12:56:14 -0400

    Add Key to KMS/Service type

--- aws/kms/service.go
+++ aws/kms/service.go
@@ -2,17 +2,20 @@ package kms
 
 import (
 	"github.com/GSA/grace-tftest/aws/kms/alias"
+	"github.com/GSA/grace-tftest/aws/kms/key"
 	"github.com/aws/aws-sdk-go/aws/client"
 )
 
 // Service contains all the supported types for S3
 type Service struct {
 	Alias *alias.Alias
+	Key   *key.Key
 }
 
 // New returns a new *Service
 func New(client client.ConfigProvider) *Service {
 	return &Service{
 		Alias: alias.New(client),
+		Key:   key.New(client),
 	}
 }
